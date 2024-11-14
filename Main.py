c97f27c934940bd9cae484331cbbf12ab4d57018
Anna <chernysh.anna06@gmail.com>
2024-11-14 22:27:11 +0000

    almost done interface

--- Main.py
+++ Main.py
@@ -1,19 +1,7 @@
-from Persons import Person
-from FamilyTree import FamilyTree
-from datetime import datetime
+import tkinter as tk
+from interface import AppInterface
 
-grandfather = Person("Walter Emmerson", birth_date=(19), death_date=())
-grandmother = Person("Anna Emmerson", birth_date=(), death_date=())
-father = Person("Otto Emmerson", birth_date=(), parents = [grandfather,grandmother])
-mother = Person("Cornelia Emmerson", birth_date=())
-child1 = Person("Alex Emmerson", parents = [mother, father])
-child2 = Person("Luke Emmerson", birth_date=(), parents = [mother, father])
-
-grandfather.add_child(father)
-father.add_child(child1,child2)
-FamilyTree.add_members(grandmother, grandfather, mother, father, child1, child2)
-
-print("Parents of Otto Emmerson are", FamilyTree.find_parents(father,mother))
-print("Grandchildren of Walter Emmerson are", FamilyTree.find_grandchildren(grandfather))
-print("The immediate family of Otto Emmerson are", FamilyTree.find_immediate_family(father))
-print("The extended family of Alex Emmerson are", FamilyTree.find_extended_family(child1))
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AppInterface(root)
+    root.mainloop()
