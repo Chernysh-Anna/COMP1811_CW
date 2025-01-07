589710bf15c5f103b2779f99935b777e1140a13d
Anna <144003173+Chernysh-Anna@users.noreply.github.com>
2025-01-07 20:52:07 +0000

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from family_data import family_data    #so we get actual data and not just module
-from models import FamilyTree, Statistics
-
-def load_family_data():
-    family_tree = FamilyTree(family_data)
-    statistics = Statistics(family_tree)
-    return family_tree, statistics
-
-
